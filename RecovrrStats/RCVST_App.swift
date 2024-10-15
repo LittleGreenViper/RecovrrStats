969488e7abd110b304ff0c82b9573c52d821f38e
Chris Marshall <chris@riftvalleysoftware.com>
2024-10-15 09:41:36 -0400

    Initial Check-In

---
+++ RecovrrStats/RCVST_App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct RCVST_App: App {
+    var body: some Scene {
+        WindowGroup {
+            RCVST_ContentView()
+        }
+    }
+}
---
+++ RecovrrStats/RCVST_ContentView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct RCVST_ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    RCVST_ContentView()
+}
